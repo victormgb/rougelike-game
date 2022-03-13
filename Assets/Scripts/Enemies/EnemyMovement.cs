a4e0d73e99257394ffb11343f632bf669f688a00
unknown <victormgb3099@gmail.com>
2022-03-13 16:08:12 -0400

    cambios bruscos con deltaTime

--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -4,15 +4,83 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    //private Transform moveSpot;
+    private Vector2 moveSpot;
+    private Vector2 vectorPosition;
+    private bool decisionTime;
+
+    
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
+        waitTime = startWaitTime;
+        vectorPosition = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+        moveSpot = new Vector2((transform.position.x + vectorPosition.x), (transform.position.y + vectorPosition.y));
+        decisionTime = true;
+    }
+
+
+    private void Update()
+    {
+
+        if (Random.Range(0, 2) == 0)
+        {
+            Debug.Log("moving");
+            moveToOtherPosition();
+        } else
+        {
+            Debug.Log("Attacking");
+            attack();
+        }
+            
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void moveToOtherPosition()
     {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpot, speed * Time.deltaTime);
         
+        if (Vector2.Distance(transform.position, moveSpot) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                vectorPosition = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+                moveSpot = new Vector2((transform.position.x + vectorPosition.x), (transform.position.y + vectorPosition.y));
+                waitTime = startWaitTime;
+                //decisionTime = true;
+                
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+                //decisionTime = false;
+            }
+        }
+    }
+
+    void attack()
+    {
+        if (waitTime <= 0)
+        {
+            //decisionTime = true;
+
+        }
+        else
+        {
+            waitTime -= Time.deltaTime;
+            //decisionTime = false;
+        }
     }
 }
